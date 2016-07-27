52c11e5e21abbd292416592ddbb834c7aee24efd
anonymous <anonymous>
2016-07-27 13:34:41 -0400

    Test: I + X = XI

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,10 +1,10 @@
 #include <string.h>
 
-char *romanSymbols[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
-int romanSymbolLength[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2, 1};
+char *romanSymbols[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI"};
+int romanSymbolLength[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2, 1, 2};
 
 int romanToDecimal(char *roman) {
-	for(int i=1; i<=10; ++i) {
+	for(int i=1; i<=11; ++i) {
 		if (!strcmp(roman, romanSymbols[i])) {
 			return i;
 		}
