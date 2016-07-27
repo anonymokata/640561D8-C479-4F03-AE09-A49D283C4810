379e457f5f25303ba892d236c293981b277f3b67
anonymous <anonymous>
2016-07-27 10:13:15 -0400

    Test: I + II = III

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,11 +1,23 @@
 #include <string.h>
 
 size_t addRomanNumerals(char *term1, char *term2, char *sumBuffer, size_t sumBufferLength) {
-	size_t usedSumBufferLength = 2;
-	char sumRomanNumeral[] = "II";
+	size_t usedSumBufferLength;
 
-	if(usedSumBufferLength <= sumBufferLength){
-		strncpy(sumBuffer, sumRomanNumeral, sumBufferLength);
+	if(!strcmp(term1,"I") && !strcmp(term2,"I")) {
+		char sumRomanNumeral[] = "II";
+		usedSumBufferLength = 2;
+
+		if(usedSumBufferLength <= sumBufferLength){
+			strncpy(sumBuffer, sumRomanNumeral, sumBufferLength);
+		}
+	}
+	else if (!strcmp(term1,"I") && !strcmp(term2,"II")) {
+		char sumRomanNumeral[] = "III";
+		usedSumBufferLength = 3;
+
+		if(usedSumBufferLength <= sumBufferLength){
+			strncpy(sumBuffer, sumRomanNumeral, sumBufferLength);
+		}
 	}
 
 	return usedSumBufferLength;
