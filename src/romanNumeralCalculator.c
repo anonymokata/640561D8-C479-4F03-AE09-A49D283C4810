b3dc18d7f468753b092585efc29e6c22716d8e81
anonymous <anonymous>
2016-07-27 20:27:37 -0400

    Refactored to implement approach to accomidate larger numbers

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,21 +1,58 @@
 #include <string.h>
 
-char *romanSymbols[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII"};
-int romanSymbolLength[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2, 1, 2, 3, 4};
+char *onesRomanSymbols[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+char *tensRomanSymbols[] = { "", "X"};
+int romanSymbolLength[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2};
+int symbolSearchOrder[] = {0, 9, 8, 7, 6, 4, 5, 3, 2, 1};
 
 int romanToDecimal(char *roman) {
-	for(int i=1; i<=13; ++i) {
-		if (!strcmp(roman, romanSymbols[i])) {
-			return i;
+	char *foundRomanPointer;
+	char *shiftingRomanPointer;
+	int decimal = 0;
+	int i;
+	int symbolWasFound;
+
+	shiftingRomanPointer = roman;
+
+	symbolWasFound = 0;
+	for(i=1; i<=1 && !symbolWasFound; ++i) {
+		foundRomanPointer = strstr(shiftingRomanPointer, tensRomanSymbols[i]);
+		if(foundRomanPointer != NULL) {
+			symbolWasFound = 1;
+			if(shiftingRomanPointer == foundRomanPointer) {
+				decimal += i*10;
+				shiftingRomanPointer = foundRomanPointer + 1;
+			}
 		}
 	}
+
+	symbolWasFound = 0;
+	for(i=1; i<=9 && !symbolWasFound; ++i) {
+		foundRomanPointer = strstr(shiftingRomanPointer, onesRomanSymbols[symbolSearchOrder[i]]);
+		if(foundRomanPointer != NULL) {
+			symbolWasFound = 1;
+			decimal += symbolSearchOrder[i];
+		}
+	}
+
+	return decimal;
 }
 
 size_t decimalToRoman(int decimal, char *decimalBuffer, size_t decimalBufferLength) {
-	if(romanSymbolLength[decimal] <= decimalBufferLength){
-		strncpy(decimalBuffer, romanSymbols[decimal], decimalBufferLength);
+	size_t romanLength = 0;
+	int tensDigit = decimal/10;
+	int onesDigit = decimal - tensDigit*10;
+
+	if((romanSymbolLength[tensDigit] + romanLength) <= decimalBufferLength){
+		strncpy(decimalBuffer, tensRomanSymbols[tensDigit], decimalBufferLength);
+		romanLength += romanSymbolLength[tensDigit];
 	}
-	return romanSymbolLength[decimal];
+	if((romanSymbolLength[onesDigit] + romanLength) <= decimalBufferLength){
+		strcat(decimalBuffer, onesRomanSymbols[onesDigit]);
+		romanLength += romanSymbolLength[onesDigit];
+	}
+
+	return romanLength;
 }
 
 size_t addRomanNumerals(char *term1, char *term2, char *sumBuffer, size_t sumBufferLength) {
