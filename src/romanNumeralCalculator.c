92f27898f3a9d5535414566d1515cb4d5a667b5d
anonymous <anonymous>
2016-07-27 20:36:25 -0400

    Refactored to improve readability.

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,54 +1,56 @@
 #include <string.h>
 
+typedef enum { false, true } bool;
+
 char *onesRomanSymbols[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 char *tensRomanSymbols[] = { "", "X"};
 int romanSymbolLength[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2};
 int symbolSearchOrder[] = {0, 9, 8, 7, 6, 4, 5, 3, 2, 1};
 
-int romanToDecimal(char *roman) {
+int romanToArabic(char *roman) {
 	char *foundRomanPointer;
 	char *shiftingRomanPointer;
-	int decimal = 0;
+	int arabic = 0;
 	int i;
-	int symbolWasFound;
+	bool symbolWasFound;
 
 	shiftingRomanPointer = roman;
 
-	symbolWasFound = 0;
+	symbolWasFound = false;
 	for(i=1; i<=1 && !symbolWasFound; ++i) {
 		foundRomanPointer = strstr(shiftingRomanPointer, tensRomanSymbols[i]);
 		if(foundRomanPointer != NULL) {
-			symbolWasFound = 1;
+			symbolWasFound = true;
 			if(shiftingRomanPointer == foundRomanPointer) {
-				decimal += i*10;
+				arabic += i*10;
 				shiftingRomanPointer = foundRomanPointer + 1;
 			}
 		}
 	}
 
-	symbolWasFound = 0;
+	symbolWasFound = false;
 	for(i=1; i<=9 && !symbolWasFound; ++i) {
 		foundRomanPointer = strstr(shiftingRomanPointer, onesRomanSymbols[symbolSearchOrder[i]]);
 		if(foundRomanPointer != NULL) {
-			symbolWasFound = 1;
-			decimal += symbolSearchOrder[i];
+			symbolWasFound = true;
+			arabic += symbolSearchOrder[i];
 		}
 	}
 
-	return decimal;
+	return arabic;
 }
 
-size_t decimalToRoman(int decimal, char *decimalBuffer, size_t decimalBufferLength) {
+size_t arabicToRoman(int arabic, char *arabicBuffer, size_t arabicBufferLength) {
 	size_t romanLength = 0;
-	int tensDigit = decimal/10;
-	int onesDigit = decimal - tensDigit*10;
+	int tensDigit = arabic/10;
+	int onesDigit = arabic - tensDigit*10;
 
-	if((romanSymbolLength[tensDigit] + romanLength) <= decimalBufferLength){
-		strncpy(decimalBuffer, tensRomanSymbols[tensDigit], decimalBufferLength);
+	if((romanSymbolLength[tensDigit] + romanLength) <= arabicBufferLength){
+		strncpy(arabicBuffer, tensRomanSymbols[tensDigit], arabicBufferLength);
 		romanLength += romanSymbolLength[tensDigit];
 	}
-	if((romanSymbolLength[onesDigit] + romanLength) <= decimalBufferLength){
-		strcat(decimalBuffer, onesRomanSymbols[onesDigit]);
+	if((romanSymbolLength[onesDigit] + romanLength) <= arabicBufferLength){
+		strcat(arabicBuffer, onesRomanSymbols[onesDigit]);
 		romanLength += romanSymbolLength[onesDigit];
 	}
 
@@ -56,11 +58,11 @@ size_t decimalToRoman(int decimal, char *decimalBuffer, size_t decimalBufferLeng
 }
 
 size_t addRomanNumerals(char *term1, char *term2, char *sumBuffer, size_t sumBufferLength) {
-	int term1Decimal = romanToDecimal(term1);
-	int term2Decimal = romanToDecimal(term2);
-	int sumDecimal = term1Decimal + term2Decimal;
+	int term1Arabic = romanToArabic(term1);
+	int term2Arabic = romanToArabic(term2);
+	int sumArabic = term1Arabic + term2Arabic;
 
-	size_t usedSumBufferLength = decimalToRoman(sumDecimal, sumBuffer, sumBufferLength);
+	size_t usedSumBufferLength = arabicToRoman(sumArabic, sumBuffer, sumBufferLength);
 
 	return usedSumBufferLength;
 }
